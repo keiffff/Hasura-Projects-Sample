439206b4bd47307b0b064b3409e7bc20437be5f3
kei <tenguuenglish@gmail.com>
2020-07-19 21:52:57 +0900

    :sparkles: Add auth provider

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { AuthProvider } from 'providers/Auth';
+import { auth0Config } from 'variants/authConfig';
 import { routes } from 'constants/routes';
 import { ErrorBoundary } from 'components/Errorboundary';
 import { LoadingIndicator } from 'components/LoadingIndicator';
@@ -12,18 +14,25 @@ export const App = () => (
   <BrowserRouter>
     <ErrorBoundary>
       <Suspense fallback={<LoadingIndicator />}>
-        <Switch>
-          <Route path={routes.home} exact>
-            <Home />
-          </Route>
-          <Route path={routes.articles}>
-            <Articles />
-          </Route>
-          <Route path={routes.profile}>
-            <Profile />
-          </Route>
-          <Redirect to={routes.home} />
-        </Switch>
+        <AuthProvider
+          domain={auth0Config.domain}
+          client_id={auth0Config.clientId}
+          redirect_uri={window.location.origin}
+          audience={auth0Config.audience}
+        >
+          <Switch>
+            <Route path={routes.home} exact>
+              <Home />
+            </Route>
+            <Route path={routes.articles}>
+              <Articles />
+            </Route>
+            <Route path={routes.profile}>
+              <Profile />
+            </Route>
+            <Redirect to={routes.home} />
+          </Switch>
+        </AuthProvider>
       </Suspense>
     </ErrorBoundary>
   </BrowserRouter>
---
+++ packages/web/src/providers/Auth.tsx
@@ -0,0 +1,76 @@
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import createAuth0Client, {
+  Auth0Client,
+  Auth0ClientOptions,
+  RedirectLoginOptions,
+  GetTokenSilentlyOptions,
+  LogoutOptions,
+} from '@auth0/auth0-spa-js';
+import { useHistory } from 'react-router-dom';
+
+type User = {
+  name: string;
+  email: string;
+  picture: string;
+  sub: string;
+};
+
+type Auth0Context = {
+  authenticated: boolean;
+  user: User;
+  loading: boolean;
+  loginWithRedirect(options: RedirectLoginOptions): Promise<void>;
+  getTokenSilently(options?: GetTokenSilentlyOptions): Promise<string | undefined>;
+  logout(options?: LogoutOptions): void;
+};
+
+type Props = { children: ReactNode } & Auth0ClientOptions;
+
+const AuthContext = createContext((null as unknown) as Auth0Context);
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export const AuthProvider = ({ children, ...initOptions }: Props) => {
+  const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState((null as unknown) as User);
+  const [client, setClient] = useState((null as unknown) as Auth0Client);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const initAuth0 = async () => {
+      const auth0FromHook = await createAuth0Client(initOptions);
+      setClient(auth0FromHook);
+      if (window.location.search.includes('code=')) {
+        const { appState } = await auth0FromHook.handleRedirectCallback();
+        history.push(appState?.targetUrl || window.location.pathname);
+      }
+      const responseIsAuthenticated = await auth0FromHook.isAuthenticated();
+      setAuthenticated(responseIsAuthenticated);
+      if (responseIsAuthenticated) {
+        const responseGetUser = await auth0FromHook.getUser();
+        setUser(responseGetUser);
+      }
+      setLoading(false);
+    };
+    initAuth0();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authenticated,
+        user,
+        loading,
+        loginWithRedirect: (options: RedirectLoginOptions) => client.loginWithRedirect(options),
+        getTokenSilently: (options?: GetTokenSilentlyOptions) => client.getTokenSilently(options),
+        logout: (options?: LogoutOptions) => client.logout(options),
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
